82e25b0e837685282ad1602b769c76e3f9af9ed4
xyz <xuyz@yealink.com>
2019-11-06 13:51:31 +0800

    erperiment tag&log

--- java/src/main/java/lesson01/exercise/Hello.java
+++ java/src/main/java/lesson01/exercise/Hello.java
@@ -15,7 +15,9 @@ import io.jaegertracing.internal.JaegerTracer;
 
 public class Hello {
     public static JaegerTracer initTracer(String service) {
+    	// will add a tag [sampler.type]
         SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv().withType("const").withParam(1);
+        // will add a tag [sampler.param]
         ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true);
         Configuration config = new Configuration(service).withSampler(samplerConfig).withReporter(reporterConfig);
         return config.getTracer();
@@ -29,15 +31,15 @@ public class Hello {
 
     private void sayHello(String helloTo) {
     	// new span
-        Span span = tracer.buildSpan("say-hello").start();
+        Span span = tracer.buildSpan("lesson01.exercise.Hello.sayHello").start();
         // tag
-        span.setTag("tag-hello-to", helloTo);
+        span.setTag("add a tag", helloTo);
         String helloStr = String.format("Hello, %s!", helloTo);
         // log
-        span.log(ImmutableMap.of("event", "string-format", "value", helloStr));
+        span.log(ImmutableMap.of("event", "begin println", "anykey", helloStr));
         System.out.println(helloStr);
         // log
-        span.log(ImmutableMap.of("event", "println"));
+        span.log(ImmutableMap.of("event", "after println"));
         
         // finish span
         span.finish();
@@ -48,7 +50,7 @@ public class Hello {
             throw new IllegalArgumentException("Expecting one argument");
         }
         String helloTo = args[0];
-        Tracer tracer = initTracer("hello-world");
+        Tracer tracer = initTracer("lesson1");
         new Hello(tracer).sayHello(helloTo);
         //new Hello(GlobalTracer.get()).sayHello(helloTo);
     }
