fe161aa839c311a82c5bf7120235baead1cde27d
xyz <xuyz@yealink.com>
2019-11-06 16:22:16 +0800

    formatter&publisher shoud also be instrument

--- java/src/main/java/lesson03/exercise/Formatter.java
+++ java/src/main/java/lesson03/exercise/Formatter.java
@@ -4,23 +4,40 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+
+import com.google.common.collect.ImmutableMap;
 
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
+import io.jaegertracing.internal.JaegerTracer;
+import io.opentracing.Scope;
+import io.opentracing.Tracer;
+import lib.Tracing;
 
 public class Formatter extends Application<Configuration> {
+    private final Tracer tracer;
+
+    private Formatter(Tracer tracer) {
+        this.tracer = tracer;
+    }
 
     @Path("/format")
     @Produces(MediaType.TEXT_PLAIN)
     public class FormatterResource {
 
         @GET
-        public String format(@QueryParam("helloTo") String helloTo) {
-            String helloStr = String.format("Hello, %s!", helloTo);
-            return helloStr;
+        public String format(@QueryParam("helloTo") String helloTo, @Context HttpHeaders httpHeaders) {
+            try (Scope scope = Tracing.startServerSpan(tracer, httpHeaders, "format")) {
+                String helloStr = String.format("Hello, %s!", helloTo);
+                scope.span().log(ImmutableMap.of("event", "string-format", "value", helloStr));
+                return helloStr;
+            }
         }
+
     }
 
     @Override
@@ -31,6 +48,7 @@ public class Formatter extends Application<Configuration> {
     public static void main(String[] args) throws Exception {
         System.setProperty("dw.server.applicationConnectors[0].port", "8081");
         System.setProperty("dw.server.adminConnectors[0].port", "9081");
-        new Formatter().run(args);
+
+        new Formatter(Tracing.init("formatter")).run(args);
     }
 }
--- java/src/main/java/lesson03/exercise/Publisher.java
+++ java/src/main/java/lesson03/exercise/Publisher.java
@@ -4,22 +4,38 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+
+import com.google.common.collect.ImmutableMap;
 
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
+import io.opentracing.Scope;
+import io.opentracing.Tracer;
+import lib.Tracing;
 
 public class Publisher extends Application<Configuration> {
+
+    private final Tracer tracer;
+
+    private Publisher(Tracer tracer) {
+        this.tracer = tracer;
+    }
 
     @Path("/publish")
     @Produces(MediaType.TEXT_PLAIN)
     public class PublisherResource {
 
         @GET
-        public String format(@QueryParam("helloStr") String helloStr) {
-            System.out.println(helloStr);
-            return "published";
+        public String format(@QueryParam("helloStr") String helloStr, @Context HttpHeaders httpHeaders) {
+            try (Scope scope = Tracing.startServerSpan(tracer, httpHeaders, "publish")) {
+                System.out.println(helloStr);
+                scope.span().log(ImmutableMap.of("event", "println", "value", helloStr));
+                return "published";
+            }
         }
     }
 
@@ -31,6 +47,7 @@ public class Publisher extends Application<Configuration> {
     public static void main(String[] args) throws Exception {
         System.setProperty("dw.server.applicationConnectors[0].port", "8082");
         System.setProperty("dw.server.adminConnectors[0].port", "9082");
-        new Publisher().run(args);
+
+        new Publisher(Tracing.init("publisher")).run(args);
     }
 }
