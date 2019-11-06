e36b66835283861318e07e7160717ad9ab6e5411
xyz <xuyz@yealink.com>
2019-11-06 15:55:57 +0800

    start lesson3

--- java/src/main/java/lesson03/exercise/Hello.java
+++ java/src/main/java/lesson03/exercise/Hello.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import com.google.common.collect.ImmutableMap;
 
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
 import io.jaegertracing.internal.JaegerTracer;
-import io.opentracing.Scope;
+import io.opentracing.Span;
 import io.opentracing.Tracer;
-import lib.Tracing;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -37,40 +39,72 @@ public class Hello {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }     
+            
+
+    public static JaegerTracer initTracer(String service) {
+    	// will add a tag [sampler.type]
+        SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv().withType("const").withParam(1);
+        // will add a tag [sampler.param]
+        ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true);
+        Configuration config = new Configuration(service).withSampler(samplerConfig).withReporter(reporterConfig);
+        return config.getTracer();
+    }    
+    private String formatString(Span root_span, String helloTo) {
+    	Span span = tracer.buildSpan("formatString").asChildOf(root_span).start();
+    	
+        
+        String helloStr = getHttp(8081, "format", "helloTo", helloTo);
+        
+        span.log(ImmutableMap.of("event", "format string", "anykey", helloStr));
+        span.finish();
+        return helloStr;
     }
 
+    private void printHello(Span root_span, String helloStr) {
+    	Span span = tracer.buildSpan("printHello").asChildOf(root_span).start();
+        // log
+        span.log(ImmutableMap.of("event", "begin println, and sleep 10s", "anykey", helloStr));
+        System.out.println(helloStr);
+        getHttp(8082, "publish", "helloStr", helloStr);
+        nsleep(span);
+        // log
+        span.log(ImmutableMap.of("event", "after println"));
+        span.finish();
+    }
+    private void nsleep(Span parent) {
+    	Span span = tracer.buildSpan("nsleep").asChildOf(parent).start();
+    	span.log(ImmutableMap.of("event", "begin sleep"));
+        try {
+        	Thread.sleep(10000);
+        }
+        catch(Exception e) {
+        	e.printStackTrace();
+        }
+        span.log(ImmutableMap.of("event", "end sleep"));
+        span.finish();
+    }
     private void sayHello(String helloTo) {
-        try (Scope scope = tracer.buildSpan("say-hello").startActive(true)) {
-            scope.span().setTag("hello-to", helloTo);
-
-            String helloStr = formatString(helloTo);
-            printHello(helloStr);
-        }
-    }
-
-    private String formatString(String helloTo) {
-        try (Scope scope = tracer.buildSpan("formatString").startActive(true)) {
-            String helloStr = getHttp(8081, "format", "helloTo", helloTo);
-            scope.span().log(ImmutableMap.of("event", "string-format", "value", helloStr));
-            return helloStr;
-        }
-    }
-
-    private void printHello(String helloStr) {
-        try (Scope scope = tracer.buildSpan("printHello").startActive(true)) {
-            getHttp(8082, "publish", "helloStr", helloStr);
-            scope.span().log(ImmutableMap.of("event", "println"));
-        }
+    	// new span
+        Span span = tracer.buildSpan("lesson01.exercise.Hello.sayHello").start();
+        // tag
+        span.setTag("add a tag", helloTo);
+        
+        
+        String helloStr = formatString(span, helloTo);
+        printHello(span, helloStr);
+        
+        // finish span
+        span.finish();
     }
 
     public static void main(String[] args) {
         if (args.length != 1) {
             throw new IllegalArgumentException("Expecting one argument");
         }
-
         String helloTo = args[0];
-        try (JaegerTracer tracer = Tracing.init("hello-world")) {
-            new Hello(tracer).sayHello(helloTo);
-        }
+        Tracer tracer = initTracer("lesson3");
+        new Hello(tracer).sayHello(helloTo);
     }
+                    
 }
